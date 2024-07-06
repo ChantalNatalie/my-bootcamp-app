af2469991e7607a932d443509b783d4ad6fb56aa
ChantalNatalie <richardschantal8@gmail.com>
2024-07-06 13:38:14 +0100

    updates
    tweaks to supportinfo.js and css

--- src/SupportInfo/SupportInfo.js
+++ src/SupportInfo/SupportInfo.js
@@ -3,14 +3,17 @@ import './SupportInfo.css';
 
 const SupportInfo = ({ items }) => {
   return (
-    <div className="support-info">
-      {items.map((item, index) => (
-        <div key={index} className="support-item">
-          <div className="icon">{item.icon}</div>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
-        </div>
-      ))}
+    <div className="support-info-container">
+      <h2 className="support-heading">Bespoke all round <span>support!</span></h2>
+      <div className="support-info">
+        {items.map((item, index) => (
+          <div key={index} className="support-item">
+            <div className="icon">{item.icon}</div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
